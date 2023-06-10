ec6c4c4e50a39759a29bc6a3e5648d0e90561206
Priya Mohan <priyamohan17@ufl.edu>
2023-06-10 17:23:38 -0400

    completed project

---
+++ queries/02-alias.sql
@@ -0,0 +1,2 @@
+-- Write a query that selects name from albums and aliases it to "album_name"
+SELECT name as "album_name" FROM albums;
---
+++ queries/03-count-albums.sql
@@ -0,0 +1,3 @@
+-- Write a query to count the amount of albums in the albums table
+-- alias the count to "number_of_albums"
+SELECT count(*) AS "number_of_albums" FROM albums;
